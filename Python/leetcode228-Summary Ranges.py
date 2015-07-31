46d5c61fc8f05a46f77d272dcad50d8a17c59189
Gao Hongyuan <gao0927@gmail.com>
2015-07-31 19:54:21 +0800

    add 228

---
+++ Python/leetcode228-Summary Ranges.py
@@ -0,0 +1,25 @@
+# LeetCode #228
+# Summary Ranges
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {string[]}
+    def summaryRanges(self, nums):
+        l = len(nums)
+        strlist = []
+        i = 0
+
+        while i < l:
+            x = nums[i]
+            s = str(x)
+            i += 1
+            if i < l and nums[i] - x == 1:
+                while i < l and nums[i] - x == 1:
+                    # in serial
+                    x = nums[i]
+                    i += 1
+                s += '->' + str(x)
+
+            strlist.append(s)
+
+        return strlist
